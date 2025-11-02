05fd2fd707040ceb81366157ed34211bbdb57d31
sjifhf <sjifhf@gmail.com>
2025-11-03 01:26:14 +0800

    itemSpeed_patch_01

--- Assets/script/itemcontroll/getitemvisible.cs
+++ Assets/script/itemcontroll/getitemvisible.cs
@@ -7,7 +7,7 @@ public class getitemvisible : MonoBehaviour
     private getitem jump;
     public int jumpi = 1;
     public Vector3[] itemplace;
-    public int itemnumber,save,load; //��o����,�s���U��m,�^�ǭ��m
+    public int itemnumber, save, load; //��o����,�s���U��m,�^�ǭ��m
     public GameObject jumpitem;
     public GameObject jumpbuff; //�O�_��o���~
     // Start is called before the first frame update
@@ -18,7 +18,7 @@ public class getitemvisible : MonoBehaviour
         itemplace[0] = new Vector3(163, -44, 0);
         itemplace[1] = new Vector3(227, -44, 0);
         itemplace[2] = new Vector3(290, -44, 0);
-        itemplace[3] = new Vector3(355 -44, 0);
+        itemplace[3] = new Vector3(355 - 44, 0);
         itemplace[4] = new Vector3(420, -44, 0);
         itemplace[5] = new Vector3(483, -44, 0);
         jump = jumpbuff.GetComponent<getitem>();
@@ -29,25 +29,25 @@ public class getitemvisible : MonoBehaviour
     void Update()
     {
         //��o���~���
-        if(jump.activejump == 0 && jumpi == 1 && itemnumber < 6)
+        if (jump.activejump == 0 && jumpi == 1 && itemnumber < 6)
         {
-            jumpitem.SetActive(true);   
+            jumpitem.SetActive(true);
             jumpitem.transform.position = itemplace[itemnumber];
             jumpi = itemnumber;
-            itemnumber ++;
+            itemnumber++;
             load = 1;
             jumpitem.transform.SetParent(transform);
 
-            if(transform.position.y >= 65)
+            if (transform.position.y >= 65)
             {
-                jumpitem.transform.position += new Vector3(0,300,0);
+                jumpitem.transform.position += new Vector3(0, 300, 0);
             }
         }
         //..........
 
 
         //���~����
-        if(jumpi != 1 && jump.activejump == 1)
+        if (jumpi != 1 && jump.activejump == 1)
         {
             save = itemnumber;
             itemnumber = jumpi;
@@ -55,9 +55,9 @@ public class getitemvisible : MonoBehaviour
 
 
         //���~�ɤW�Ŧ�ᶶ�Ƿ���
-        if(load == 1)
+        if (load == 1)
         {
-            if(save != 0)
+            if (save != 0)
             {
                 itemnumber = save;
             }
--- Assets/script/itemcontroll/itemsortappear.cs
+++ Assets/script/itemcontroll/itemsortappear.cs
@@ -6,36 +6,36 @@ public class itemsortappear : MonoBehaviour
 {
     public float speed = 10f;          // ���~�X�{�t��
     public bool invisible = true;    // ���~�O�_����
-    public Vector3 origform,finalpos;
-    public bool upactive = false,downactive = false;
+    public Vector3 origform, finalpos;
+    public bool upactive = false, downactive = false;
     // Start is called before the first frame update
     void Start()
     {
         invisible = true;
         origform = transform.position;
-        finalpos = origform + new Vector3(0,300,0);
+        finalpos = origform + new Vector3(0, 300, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I))
         {
-            if(invisible)
+            if (invisible)
             {
                 upactive = true;
             }
-            else if(!invisible)
+            else if (!invisible)
             {
                 downactive = true;
             }
 
         }
 
-        if(upactive)
+        if (upactive)
         {
             transform.position = Vector3.MoveTowards(transform.position, finalpos, speed * Time.deltaTime);
-            if(transform.position == finalpos)
+            if (transform.position == finalpos)
             {
                 upactive = false;
                 invisible = false;
